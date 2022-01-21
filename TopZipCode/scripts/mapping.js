ad4a9a1e87e2e7459ab92ad69904fd429fa510e5
peterchan032 <97752776+peterchan032@users.noreply.github.com>
2022-01-21 11:02:36 -0800

    Add files via upload

--- TopZipCode/scripts/mapping.js
+++ TopZipCode/scripts/mapping.js
@@ -1,3 +1,5 @@
+console.log('Top Zip Codes!');
+
 /*------------------ load the map ---------------*/
 		mapboxgl.accessToken = 'pk.eyJ1IjoicGxhY2VybWFwdGVhbSIsImEiOiJja3d6NTh4aGkwa2pwMndvNjRnZDR2Zmc0In0.8J8KJ5IxdOF3gTEs8HVM2Q';
       	const map = new mapboxgl.Map({
